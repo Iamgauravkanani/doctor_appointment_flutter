4dd45711ce67ff8debcc70c333e21a2fe3c2d062
chirag9710 <chiragkalathiya129@gmail.com>
2024-03-02 20:07:51 +0530

    background layer

--- lib/modules/screens/login.dart
+++ lib/modules/screens/login.dart
@@ -19,175 +19,206 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: Get.height * 0.2,
+      body: Stack(
+        children: [
+          Image.asset(
+            "asset/image/layer_blur.png",
+            width: Get.width * .7,
+          ),
+          Positioned(
+            bottom: -20,
+            right: 0,
+            child: Image.asset(
+              "asset/image/bottom_layer.png",
+              height: Get.height * .4,
+              width: Get.width * .7,
             ),
-            Text(
-              "Welcome back",
-              style: rubik(fontSize: 24, fontWeight: FontWeight.w500),
-            ),
-            SizedBox(
-              height: Get.height * 0.02,
-            ),
-            Text(
-              "You can search c ourse, apply course and find\n scholarship for abroad studies",
-              textAlign: TextAlign.center,
-              style: rubik(
-                fontWeight: FontWeight.w400,
-                color: textPrimary,
+          ),
+          Column(
+            children: [
+              SizedBox(
+                height: Get.height * 0.2,
               ),
-            ),
-            SizedBox(
-              height: Get.height * 0.05,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 54,
-                  width: 160,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(Assets.google),
-                      SizedBox(
-                        width: Get.width * 0.04,
-                      ),
-                      Text(
-                        "Google",
-                        style: rubik(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 16,
-                            color: textPrimary),
-                      ),
-                    ],
-                  ),
+              Text(
+                "Welcome back",
+                style: rubik(fontSize: 24, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: Get.height * 0.02,
+              ),
+              Text(
+                "You can search c ourse, apply course and find\n scholarship for abroad studies",
+                textAlign: TextAlign.center,
+                style: rubik(
+                  fontWeight: FontWeight.w400,
+                  color: textPrimary,
                 ),
-                SizedBox(
-                  width: Get.width * 0.05,
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 54,
-                  width: 160,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(Assets.fb),
-                      SizedBox(
-                        width: Get.width * 0.04,
-                      ),
-                      Text(
-                        "FaceBook",
-                        style: rubik(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 16,
-                            color: textPrimary),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: Get.height * 0.05,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
+              ),
+              SizedBox(
+                height: Get.height * 0.05,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    cursorColor: black,
-                    decoration: InputDecoration(
-                      hintText: 'itsmemamun1@gmail.com',
-                      hintStyle: rubik(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                          color: textPrimary),
-                      enabledBorder: outlineInputBorder(radius: 4),
-                      focusedBorder: outlineInputBorder(radius: 4),
-                      border: outlineInputBorder(radius: 4),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 54,
+                    width: 160,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(.04),
+                            blurRadius: 10,
+                            spreadRadius: 0),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  TextFormField(
-                    obscureText: visible,
-                    cursorColor: black,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            visible = !visible;
-                          });
-                        },
-                        icon: Icon(
-                          (visible)
-                              ? CupertinoIcons.eye
-                              : CupertinoIcons.eye_slash,
-                          color: textPrimary,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(Assets.google),
+                        SizedBox(
+                          width: Get.width * 0.04,
                         ),
-                      ),
-                      hintText: '********',
-                      hintStyle: rubik(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
-                          color: textPrimary),
-                      enabledBorder: outlineInputBorder(),
-                      focusedBorder: outlineInputBorder(),
-                      border: outlineInputBorder(),
+                        Text(
+                          "Google",
+                          style: rubik(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                              color: textPrimary),
+                        ),
+                      ],
                     ),
                   ),
                   SizedBox(
-                    height: Get.height * 0.04,
+                    width: Get.width * 0.05,
                   ),
-                  FilledButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStatePropertyAll(btnColor),
-                      shape: MaterialStatePropertyAll(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12)),
-                      ),
-                      minimumSize: MaterialStatePropertyAll(
-                        Size(Get.width * 0.85, Get.height * 0.07),
-                      ),
+                  Container(
+                    alignment: Alignment.center,
+                    height: 54,
+                    width: 160,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black.withOpacity(.04),
+                            blurRadius: 10),
+                      ],
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    onPressed: () {
-                      Get.toNamed('/nav');
-                    },
-                    child: Text(
-                      "Login",
-                      style: rubik(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 18,
-                        color: Colors.white,
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Image.asset(Assets.fb),
+                        SizedBox(
+                          width: Get.width * 0.04,
+                        ),
+                        Text(
+                          "FaceBook",
+                          style: rubik(
+                              fontWeight: FontWeight.w300,
+                              fontSize: 16,
+                              color: textPrimary),
+                        ),
+                      ],
                     ),
-                  ),
-                  SizedBox(
-                    height: Get.height * 0.01,
-                  ),
-                  Text(
-                    "Forget password",
-                    style: rubik(color: btnColor, fontWeight: FontWeight.w400),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
+              SizedBox(
+                height: Get.height * 0.05,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      cursorColor: black,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        hintText: 'itsmemamun1@gmail.com',
+                        hintStyle: rubik(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: textPrimary),
+                        enabledBorder: outlineInputBorder(),
+                        focusedBorder: outlineInputBorder(),
+                        border: outlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    TextFormField(
+                      obscureText: visible,
+                      cursorColor: black,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              visible = !visible;
+                            });
+                          },
+                          icon: Icon(
+                            (visible)
+                                ? CupertinoIcons.eye
+                                : CupertinoIcons.eye_slash,
+                            color: textPrimary,
+                          ),
+                        ),
+                        hintText: '********',
+                        hintStyle: rubik(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                            color: textPrimary),
+                        enabledBorder: outlineInputBorder(),
+                        focusedBorder: outlineInputBorder(),
+                        border: outlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(
+                      height: Get.height * 0.04,
+                    ),
+                    FilledButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(btnColor),
+                        shape: MaterialStatePropertyAll(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12)),
+                        ),
+                        minimumSize: MaterialStatePropertyAll(
+                          Size(Get.width * 0.85, Get.height * 0.07),
+                        ),
+                      ),
+                      onPressed: () {
+                        Get.toNamed('/nav');
+                      },
+                      child: Text(
+                        "Login",
+                        style: rubik(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: Get.height * 0.01,
+                    ),
+                    Text(
+                      "Forget password",
+                      style:
+                          rubik(color: btnColor, fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
